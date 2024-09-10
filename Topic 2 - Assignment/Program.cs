265eab2c714bf1be195d6a50cb3bd56dc9ceb67e
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-09-10 09:30:32 -0400

    Add project files.

---
+++ Topic 2 - Assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Topic_2___Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Carsen
+            Console.WriteLine("I was born in the year " + (2024 - 16));
+            Console.WriteLine();
+            Console.WriteLine("The average of 14, 15, and 16 is " + ((14 + 15 + 16) / 3));
+            Console.WriteLine();
+            Console.WriteLine("The average of 14, 15, 16, and 17 is " + ((14.0 + 15.0 + 16.0 +17.0) / 4));
+            Console.WriteLine();
+            Console.WriteLine("The area of a circle with a radius of 3 centimeters is " + (2024 - 16));
+        }
+    }
+}
