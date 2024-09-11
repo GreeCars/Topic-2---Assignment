3abdf550825fca028430d91a85388208dc7124c1
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-09-11 09:04:06 -0400

    Finished

--- Topic 2 - Assignment/Program.cs
+++ Topic 2 - Assignment/Program.cs
@@ -11,7 +11,12 @@
             Console.WriteLine();
             Console.WriteLine("The average of 14, 15, 16, and 17 is " + ((14.0 + 15.0 + 16.0 +17.0) / 4));
             Console.WriteLine();
-            Console.WriteLine("The area of a circle with a radius of 3 centimeters is " + (3.14 * 3.0));
+            Console.Write("The area of a circle with a radius of 3 centimeters is " + (22.0 / 7.0 * 3.0 * 3.0));
+            Console.Write("cm");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("When the + operator is being used on numbers outside of quotation marks, they will be added together. When + is adding text inside of quotations to a number on the outside, it sticks them together.");
+            Console.ReadLine();
         }
     }
 }
